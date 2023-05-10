ef7bb87b62458a5c2321dce1923f73f9dca1fd52
Daniel Okonov <daniel.okonov@gmail.com>
2023-05-09 22:27:02 -0700

    Implemented basic Add patient funcionality

---
+++ patient.js
@@ -0,0 +1,40 @@
+require("dotenv").config();
+const express = require("express");
+const bodyParser = require("body-parser");
+const { MongoClient } = require("mongodb");
+
+const app = express();
+const url = `mongodb+srv://${process.env.MONGODB_USER}:${process.env.MONGODB_PASSWORD}@${process.env.MONGODB_CLUSTER}/?retryWrites=true&w=majority`;
+const dbName = process.env.MONGODB_DATABASE;
+const client = new MongoClient(url, { useUnifiedTopology: true });
+
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.get("/add-patient", (req, res) => {
+  res.render("add-patient");
+});
+
+app.post("/add-patient", async (req, res) => {
+  await client.connect();
+
+  const db = client.db(dbName);
+  const patientsCollection = db.collection(process.env.MONGODB_COLLECTION);
+  const age = parseInt(req.body.age);
+
+  const patientData = {
+    respectiveDoctorId: req.body.respectiveDoctorId,
+    name: req.body.name,
+    age: age,
+    sex: req.body.sex,
+    previous_analysis: [],
+  };
+
+  await patientsCollection.insertOne(patientData);
+
+  res.send("Patient added successfully");
+});
+
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
