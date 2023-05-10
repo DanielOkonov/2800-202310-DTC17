3bd084fdb2111337e65373766d77e40f5322fbe6
Daniel Okonov <daniel.okonov@gmail.com>
2023-05-10 11:47:34 -0700

    Implement joi validation for patient schema

--- patient.js
+++ patient.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const { MongoClient } = require("mongodb");
+const Joi = require("joi");
 
 const app = express();
 const url = `mongodb+srv://${process.env.MONGODB_USER}:${process.env.MONGODB_PASSWORD}@${process.env.MONGODB_CLUSTER}/?retryWrites=true&w=majority`;
@@ -11,11 +12,24 @@ const client = new MongoClient(url, { useUnifiedTopology: true });
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: false }));
 
+const patientSchema = Joi.object({
+  name: Joi.string().min(1).required(),
+  age: Joi.number().min(1).required(),
+  sex: Joi.string().valid("male", "female", "other").required(),
+});
+
 app.get("/add-patient", (req, res) => {
   res.render("add-patient");
 });
 
 app.post("/add-patient", async (req, res) => {
+  const { error } = patientSchema.validate(req.body);
+
+  if (error) {
+    res.status(400).send(error.details[0].message);
+    return;
+  }
+
   await client.connect();
 
   const db = client.db(dbName);
