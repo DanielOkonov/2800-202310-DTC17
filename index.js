b3d35004a0a65f041cefb48d4dbdf48a308a308e
Daniel Okonov <daniel.okonov@gmail.com>
2023-05-10 19:20:12 -0700

    Session processing moved to index

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
+const session = require("express-session");
+const dotenv = require("dotenv");
+const MongoDBSession = require("connect-mongodb-session")(session);
 app.use(bodyParser.urlencoded({ extended: false }));
 app.set("view engine", "ejs");
+
+dotenv.config();
 
 const server = require("./server");
 const patient = require("./patient");
@@ -10,6 +15,30 @@ const patient = require("./patient");
 module.exports = app;
 
 app.set("view engine", "ejs");
+
+const url = `mongodb+srv://${process.env.MONGODB_USER}:${process.env.MONGODB_PASSWORD}@${process.env.MONGODB_CLUSTER}/?retryWrites=true&w=majority`;
+
+const mongoStore = new MongoDBSession({
+  uri: url,
+  collection: "sessions",
+});
+
+// Use the session middleware (sessions expire after 1 hour)
+
+const NODE_SESSION_SECRET = process.env.NODE_SESSION_SECRET;
+
+app.use(
+  session({
+    secret: NODE_SESSION_SECRET,
+    store: mongoStore,
+    saveUninitialized: false,
+    resave: false,
+    rolling: true,
+    cookie: {
+      maxAge: 60 * 60 * 1000, // 1 hour in milliseconds
+    },
+  })
+);
 
 app.get("/add-patient", patient.renderAddPatients);
 app.post("/add-patient", patient.addPatient);
